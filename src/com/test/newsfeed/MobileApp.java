0173fd0d8f5688cf449a492d720adc3e56514477
rajeev singh <rajeevkri@gmail.com>
2017-04-03 12:15:05 +0530

    removed redundant code

--- src/com/test/newsfeed/MobileApp.java
+++ src/com/test/newsfeed/MobileApp.java
@@ -1,8 +1,5 @@
 package com.test.newsfeed;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MobileApp extends Channel {
 
 	NewsCategory category;
@@ -15,14 +12,6 @@ public class MobileApp extends Channel {
 	@Override
 	public void notifyFeed(String feed) {
 		System.out.println("Mobile APP is getting notified with feed = " + feed);
-	}
-
-	public static void main(String[] args) {
-		List<Integer> intList = new ArrayList<>();
-		
-		intList.add(2);
-		intList.add(0, 1);
-		System.out.println(intList);
 	}
 
 	@Override
