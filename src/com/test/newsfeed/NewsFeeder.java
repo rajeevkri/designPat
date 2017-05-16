fb82d901dfe6d9b32e7f61a5c3460e390c07cc19
rajeev singh <rajeevkri@gmail.com>
2017-05-16 19:52:02 +0530

    news feed modified

--- src/com/test/newsfeed/NewsFeeder.java
+++ src/com/test/newsfeed/NewsFeeder.java
@@ -11,34 +11,35 @@ import java.util.Map;
  *
  */
 public class NewsFeeder {
-//	private List<Channel> channels = new ArrayList<>();
-	
+	// private List<Channel> channels = new ArrayList<>();
+
 	private Map<NewsCategory, List<Channel>> channelMap = new HashMap<>();
-	
+
 	private Map<NewsCategory, List<String>> feedsMap = new HashMap<>();
-	private List<String> feeds = new ArrayList<>();
-	
-	/*public List<Channel> getChannels() {
-		return channels;
-	}*/
-	
+
+	/*
+	 * public List<Channel> getChannels() { return channels; }
+	 */
+
 	public void feedNews(String feed, NewsCategory cat) {
+		List<String> feeds = feedsMap.get(cat);
+		if (feeds == null)
+			feeds = new ArrayList<>();
 		feeds.add(feed);
+		feedsMap.put(cat, feeds);
 		notifyChannels(feed, cat);
 	}
 
 	private void notifyChannels(String feed, NewsCategory cat) {
 		List<Channel> channels = channelMap.get(cat);
-		if(channels != null) {
-			for(Channel channel:channels) {
+		if (channels != null) 
+			for (Channel channel : channels)
 				channel.notifyFeed(feed);
-			}
-		}
 	}
-	
+
 	public void attachChannels(Channel channel, NewsCategory cat) {
 		List<Channel> channels = channelMap.get(cat);
-		if(channels == null) {
+		if (channels == null) {
 			channels = new ArrayList<>();
 			channelMap.put(cat, channels);
 		}
